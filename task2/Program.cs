ff764b52370c435c788af1277ba7c2ae653052c3
Andrey Vedischev <vedischev76@mail.ru>
2022-07-07 23:57:50 +0500

    task 2 other -

---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+WriteLine("Введите стартовое число: ");
+int m = Convert.ToInt32(ReadLine());
+OutputBeforeN(n, m);
+
+void OutputBeforeN(int a, int start)
+{
+    if (a > start)
+    {
+        if (start <= a)
+        {
+            Write($"{start} ");
+            start++;
+            OutputBeforeN(a, start);
+        }
+        else return;
+    }
+    
+    // else
+    // if (a < start)
+    // {
+    //     int temp = a;
+    //     a = start;
+    //     start = temp;
+    //     OutputBeforeN(a, start);
+    // }
+    // return;
+    
+}
