1a8cb949f8e3dac3532026d43533d94454bdb36c
Andrey Vedischev <vedischev76@mail.ru>
2022-07-07 23:56:45 +0500

    task 1 +

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,15 +6,21 @@ WriteLine("Введите число N: ");
 int n = Convert.ToInt32(ReadLine());
 WriteLine("Введите стартовое число: ");
 int m = Convert.ToInt32(ReadLine());
-OutputBeforeN(n,m);
+if (n < m)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+OutputBeforeN(n, m);
 
-void OutputBeforeN(int a,int start)
-{    
-    if(start <= a)
+void OutputBeforeN(int a, int start)
+{
+    if (start <= a)
     {
-        Write($"{start} ");        
+        Write($"{start} ");
         start++;
-        OutputBeforeN(a,start);       
-    }    
+        OutputBeforeN(a, start);
+    }
     else return;
 }
