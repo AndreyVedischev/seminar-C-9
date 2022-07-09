d2d204b5598d536b45a52d1568a434a8ac21dd98
Andrey Vedischev <vedischev76@mail.ru>
2022-07-09 13:47:54 +0500

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+WriteLine(SumNumbers(n));
+
+int SumNumbers(int a)
+{
+    int sum = 0;
+    if(a>0)
+    {
+        sum = a%10 + SumNumbers(a/10);   
+                        
+    }
+    return sum;    
+}
