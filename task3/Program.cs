ab4ab2b27bd45421d23c4cf200b39ed77d229f8d
Andrey Vedischev <vedischev76@mail.ru>
2022-07-09 12:54:39 +0500

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine(GetNumbers(5, 2));
+
+string GetNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    if (start < end) return $"{start} {GetNumbers(start + 1, end)}";
+    else
+    {
+        return $"{end} {GetNumbers(end + 1, start)}";
+    }    
+}
