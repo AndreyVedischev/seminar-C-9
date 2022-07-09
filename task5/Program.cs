3b580407b6cdb1c89e6b92ef8f6a92c1d9e1f4e0
Andrey Vedischev <vedischev76@mail.ru>
2022-07-09 14:23:55 +0500

    task 5

---
+++ task5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число A: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число B: ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine(MultiNumbers(a, b));
+
+int MultiNumbers(int n, int m)
+{
+    return m > 0 ? n * MultiNumbers(n, --m) : 1;
+    // int pov = 0;
+    // if(m > 0)
+    // {
+    //     return pov = n*MultiNumbers(n,--m);
+
+    // }
+    // else return 1;
+}
